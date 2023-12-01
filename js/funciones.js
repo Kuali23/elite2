d04b75f17f93907c5c085153bc3ba3c6f9665106
sistemaskuali.soporte@gmail.com <sistemaskuali.soporte@gmail.com>
2023-12-01 16:35:30 -0600

    Version 2.0 de Elite

---
+++ js/funciones.js
@@ -0,0 +1,6 @@
+// Configuración para que el carrusel cambie automáticamente cada 5 segundos
+$(document).ready(function () {
+    $('#carouselExample').carousel({
+        interval: 3000 // 5000 milisegundos = 5 segundos
+    });
+});
