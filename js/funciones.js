6513e219b0c7b5e88dc83168e0e4607fc67b80c7
sistemaskuali.soporte@gmail.com <sistemaskuali.soporte@gmail.com>
2023-12-04 19:54:34 -0600

    Inicio terminada

--- js/funciones.js
+++ js/funciones.js
@@ -4,3 +4,27 @@ $(document).ready(function () {
         interval: 3000 // 5000 milisegundos = 5 segundos
     });
 });
+
+$(document).ready(function () {
+    $('.number').waypoint(function () {
+        var $this = $(this.element);
+        var countTo = parseInt($this.attr('data-number'));
+
+        var options = {
+            useEasing: true,
+            useGrouping: true,
+            separator: ',',
+            decimal: '.',
+            prefix: '',
+            suffix: ''
+        };
+
+        var countUp = new CountUp($this[0], 0, countTo, 0, 2.5, options);
+        if (!countUp.error) {
+            countUp.start();
+        } else {
+            console.error(countUp.error);
+        }
+        this.destroy(); // Para evitar que la animación se repita al desplazarse nuevamente a través del elemento
+    }, { offset: '95%' });
+});
